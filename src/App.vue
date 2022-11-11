a09a6f28ebb626a0e076ff076cb81a4f9e2e3444
c4az6 <1762458611@qq.com>
2022-11-12 02:06:03 +0800

    解决@路径别名没有智能提示的问题

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,13 @@
     <router-view></router-view>
 
     App.vue
+
+    <tab-bar></tab-bar>
   </div>
 </template>
 
 <script setup>
-
+import TabBar from './components/tab-bar/tabbar.vue'
 </script>
 
 <style lang="less" scoped>
---
+++ src/components/tab-bar/tabbar.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="tabbar">
+    <div class="tabbar-item">
+      <img src="" alt="">
+      <span class="text">首页</span>
+    </div>
+
+    <div class="tabbar-item">
+      收藏
+    </div>
+
+    <div class="tabbar-item">
+      订单
+    </div>
+
+    <div class="tabbar-item">
+      消息
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="less" scoped>
+.tabbar {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: 50px;
+  display: flex;
+  border-top: 1px solid #f3f3f3;
+
+  .tabbar-item {
+    flex: 1;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+}
+</style>
