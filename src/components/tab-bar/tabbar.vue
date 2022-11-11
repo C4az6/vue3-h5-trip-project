b5739f2873746dfec928f79fca958dfb94feca26
c4az6 <1762458611@qq.com>
2022-11-12 02:20:59 +0800

    tabbar基础布局完成

--- src/components/tab-bar/tabbar.vue
+++ src/components/tab-bar/tabbar.vue
@@ -1,25 +1,34 @@
 <template>
   <div class="tabbar">
-    <div class="tabbar-item">
-      <img src="" alt="">
+    <div class="tabbar-item" @click="handleRouter('/home')">
+      <img src="@/assets/img/tabbar/tab_home.png" alt="">
       <span class="text">首页</span>
     </div>
 
-    <div class="tabbar-item">
-      收藏
+    <div class="tabbar-item" @click="handleRouter('/favor')">
+      <img src="@/assets/img/tabbar/tab_favor.png" alt="">
+      <span class="text">收藏</span>
     </div>
 
-    <div class="tabbar-item">
-      订单
+    <div class="tabbar-item" @click="handleRouter('/order')">
+      <img src="@/assets/img/tabbar/tab_order.png" alt="">
+      <span class="text">订单</span>
     </div>
 
-    <div class="tabbar-item">
-      消息
+    <div class="tabbar-item" @click="handleRouter('/message')">
+      <img src="@/assets/img/tabbar/tab_message.png" alt="">
+      <span class="text">消息</span>
     </div>
   </div>
 </template>
 
 <script setup>
+import { useRouter } from 'vue-router';
+const router = useRouter();
+
+const handleRouter = (link) => {
+  router.push(link)
+}
 
 </script>
 
@@ -38,6 +47,14 @@
     display: flex;
     align-items: center;
     justify-content: center;
+    img {
+      width: 26px;
+      margin-right: 2px;
+    }
+    .text {
+      padding-top: 2px;
+      font-size: 12px;
+    }
   }
 }
 </style>
