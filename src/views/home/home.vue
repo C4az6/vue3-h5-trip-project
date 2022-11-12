fcd117cca1744e7a53554f0387877bf47149a8f4
c4az6 <1762458611@qq.com>
2022-11-13 04:34:49 +0800

    首页头部区域开发

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,10 +1,20 @@
 <template>
   <div class="home">
-    <van-button type="primary">主要按钮</van-button>
-    <van-button type="success">成功按钮</van-button>
-    <van-button type="default">默认按钮</van-button>
-    <van-button type="warning">警告按钮</van-button>
-    <van-button type="danger">危险按钮</van-button>
+    <div class="nav-bar-wrap">
+      旅游伴侣
+    </div>
+
+    <div class="banner">
+      <img src="@/assets/img/home/banner.webp" alt="">
+    </div>
+
+    <div class="location">
+      <span class="city-text">广州</span>
+      <div class="position">
+        <span>我的位置</span>
+        <img src="@/assets/img/home/icon_location.png" alt="">
+      </div>
+    </div>
   </div>
 </template>
 
@@ -13,4 +23,35 @@
 </script>
 
 <style lang="less" scoped>
+.nav-bar-wrap {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 46px;
+  background-color: #fff;
+  color: var(--primary-color);
+  font-size: 16px;
+}
+
+.location {
+  display: flex;
+  align-items: center;
+  height: 45px;
+  padding: 0 20px;
+  line-height: initial;
+  .city-text {
+    font-size: 14px;
+  }
+  .position {
+    margin-left: auto;
+    display: flex;
+    align-items: center;
+    font-size: 12px;
+    img {
+      width: 18px;
+      height: 18px;
+      margin-left: 6px;
+    }
+  }
+}
 </style>
