8aeae92cf56cd6ad0ba7350dc4bfb023f77b50bb
c4az6 <1762458611@qq.com>
2022-11-13 06:42:16 +0800

    跳转城市页面和隐藏tabbar开发

---
+++ src/views/home/components/home-search-box.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="search-box">
+    <div class="location">
+      <span @click="cityClick" class="city-text">广州</span>
+      <div class="position" @click="positionClick">
+        <span>我的位置</span>
+        <img src="@/assets/img/home/icon_location.png" alt="">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router';
+const router = useRouter();
+// 位置/城市点击事件
+const positionClick = () => {
+  // 获取用户当前的经纬度
+  navigator.geolocation.getCurrentPosition(res => {
+    console.log("获取成功: ", res);
+  }, err => {
+    console.log("获取失败: ", err);
+  })
+};
+
+const cityClick = () => {
+  router.push('/city');
+}
+
+
+</script>
+
+<style lang="less" scoped>
+.location {
+  display: flex;
+  align-items: center;
+  height: 45px;
+  padding: 0 20px;
+  line-height: initial;
+  .city-text {
+    font-size: 14px;
+  }
+  .position {
+    margin-left: auto;
+    display: flex;
+    align-items: center;
+    font-size: 12px;
+    img {
+      width: 18px;
+      height: 18px;
+      margin-left: 6px;
+    }
+  }
+}
+</style>
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -3,22 +3,16 @@
     <div class="nav-bar-wrap">
       旅游伴侣
     </div>
-
     <div class="banner">
       <img src="@/assets/img/home/banner.webp" alt="">
     </div>
 
-    <div class="location">
-      <span class="city-text">广州</span>
-      <div class="position">
-        <span>我的位置</span>
-        <img src="@/assets/img/home/icon_location.png" alt="">
-      </div>
-    </div>
+    <HomeSearchBox></HomeSearchBox>
   </div>
 </template>
 
 <script setup>
+import HomeSearchBox from './components/home-search-box.vue'
 
 </script>
 
@@ -31,27 +25,5 @@
   background-color: #fff;
   color: var(--primary-color);
   font-size: 16px;
-}
-
-.location {
-  display: flex;
-  align-items: center;
-  height: 45px;
-  padding: 0 20px;
-  line-height: initial;
-  .city-text {
-    font-size: 14px;
-  }
-  .position {
-    margin-left: auto;
-    display: flex;
-    align-items: center;
-    font-size: 12px;
-    img {
-      width: 18px;
-      height: 18px;
-      margin-left: 6px;
-    }
-  }
 }
 </style>
