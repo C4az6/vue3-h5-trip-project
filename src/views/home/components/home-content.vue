4ab36caeaec885d0508db37279e423f39c0db0b0
c4az6 <1762458611@qq.com>
2022-11-17 04:50:58 +0800

    列表数据的不同类型展示

--- src/views/home/components/home-content.vue
+++ src/views/home/components/home-content.vue
@@ -2,12 +2,23 @@
   <div class="content">
     <h2 class="title">热门精选</h2>
     <div class="list">
-      列表数据
+      <template v-for="(item) in houseList" :key="item.data.houseId">
+        <div class="item">
+          <small v-if="item.discoveryContentType === 9">type-9: {{item.data.houseName}}</small>
+          <small v-else-if="item.discoveryContentType === 3">type-3: {{item.data.houseName}}</small>
+        </div>
+      </template>
     </div>
   </div>
 </template>
 
 <script setup>
+import useHomeStore from "@/stores/modules/home";
+import { storeToRefs } from "pinia";
+
+
+const homeStore = useHomeStore();
+const { houseList } = storeToRefs(homeStore);
 
 </script>
 
